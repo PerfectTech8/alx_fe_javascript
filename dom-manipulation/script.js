3a989b981e2e2274624deabef76a0e4668ebe1a9
Nnanta Charles <nnantaprince2@gmail.com>
2025-10-18 16:58:36 +0100

    added localStorage, export and import functions

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,12 +4,19 @@ const newQuoteBtn = document.getElementById("newQuote");
 const newQuoteText = document.getElementById("newQuoteText");
 const newQuoteCategory = document.getElementById("newQuoteCategory");
 const listItem = document.getElementById("listItem");
+const ExportBtn = document.getElementById("exportBtn");
+const importBtn = document.getElementById("importBtn");
+const importfile = document.getElementById("importFile");
 
 
-const quotes = [{text: "Success is not the key to happiness. Happiness is the key to success", category: "Inspiration"}, 
+// const quotes = [{text: "Success is not the key to happiness. Happiness is the key to success", category: "Inspiration"}, 
+//     {text: "The best way to get started is to quit talking and begin doing", category: "Motivation"},
+//      {text: "Don't let yesterday take up too much of today", category: "Life"}];
+
+
+     let quotes = JSON.parse(localStorage.getItem("quotes")) || [{text: "Success is not the key to happiness. Happiness is the key to success", category: "Inspiration"}, 
     {text: "The best way to get started is to quit talking and begin doing", category: "Motivation"},
-     {text: "Don't let yesterday take up too much of today", category: "Life"}];
-
+     {text: "Don't let yesterday take up too much of today", category: "Life"}]; 
      //random quotes
      function showRandomQuote(){
         const randomNumber = Math.floor(Math.random() * quotes.length);
@@ -19,7 +26,17 @@ const quotes = [{text: "Success is not the key to happiness. Happiness is the ke
      }
      newQuoteBtn.addEventListener("click", showRandomQuote);
      showRandomQuote();
-     console.log("Hello")
+
+     //display all quotes
+
+     function displayQuotes(){
+      listItem.innerHTML = "";
+      quotes.forEach((q) => {
+         const li = document.createElement("li");
+         li.textContent = `"${q.text}" - ${q.category}`;
+         listItem.appendChild(li);
+      })
+     }
 
      //user adding quotes
 
@@ -31,14 +48,75 @@ const quotes = [{text: "Success is not the key to happiness. Happiness is the ke
         return;
      }
      const updatedQuote = {text, category};
-     quotes.push(updatedQuote);
-     const li = document.createElement("li");
-     li.textContent = `"${updatedQuote.text} - ${updatedQuote.category}`;
-     listItem.appendChild(li);
+      quotes.push(updatedQuote);
+   //   const li = document.createElement("li");
+   //   li.textContent = `"${updatedQuote.text}" - ${updatedQuote.category}`;
+   //   listItem.appendChild(li);
+
+     
+     //update local storage
+     localStorage.setItem("quotes", JSON.stringify(quotes));
+
+     displayQuotes();
+     showRandomQuote();
+
      newQuoteText.value = "";
      newQuoteCategory.value = "";
+
+    }
+
+    //export function
+    function exportQuotes(){
+      const quotesFile = JSON.stringify(quotes, null, 2);
+      const blob = new Blob([quotesFile], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "quotes.json";
+      link.click();
+      setTimeout(() => {
+         URL.revokeObjectURL(url);
+      }, 2000);
+
+    }
+
+    //import function
+    function importFromJsonFile(event){
+      const files = importfile.files[0];
+      if(!files){
+         alert("import a JSON file");
+         return;
+      }
+      
+      const reader= new FileReader();
+      reader.onload = function(e){
+         try{
+         const importedQuotes = JSON.parse(e.target.result);
+         if(!Array.isArray(importedQuotes)){
+            alert("Incorrect file type!");
+            return;
+         }
+         quotes.push(...importedQuotes);
+         localStorage.setItem("quotes", JSON.stringify(quotes));
+         alert("Quotes imported successfully");
+         displayQuotes();
+         }catch(error){
+            alert("Error reading file" + error.message);
+         }
+      }
+      reader.readAsText(files);
+      
+
+   
     }
     
-    
+    //add eventlisteners
+     ExportBtn.addEventListener("click", exportQuotes);
+   //   importBtn.addEventListener("click", importFromJsonFile);
+
+     displayQuotes();
+     showRandomQuote();
+
+ 
 
 
