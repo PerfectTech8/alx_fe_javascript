fff53850f7eb40f87a6c9dcfd6417ad6c4b2b50b
Nnanta Charles <nnantaprince2@gmail.com>
2025-10-19 17:14:16 +0100

    updated the app by add filter

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,4 +1,3 @@
-
 const quoteDisplay = document.getElementById("quoteDisplay");
 const newQuoteBtn = document.getElementById("newQuote");
 const newQuoteText = document.getElementById("newQuoteText");
@@ -7,6 +6,8 @@ const listItem = document.getElementById("listItem");
 const ExportBtn = document.getElementById("exportBtn");
 const importBtn = document.getElementById("importBtn");
 const importfile = document.getElementById("importFile");
+const categoryFilter = document.getElementById("categoryFilter");
+const addBtn = document.getElementById("add");
 
 
 // const quotes = [{text: "Success is not the key to happiness. Happiness is the key to success", category: "Inspiration"}, 
@@ -17,26 +18,29 @@ const importfile = document.getElementById("importFile");
      let quotes = JSON.parse(localStorage.getItem("quotes")) || [{text: "Success is not the key to happiness. Happiness is the key to success", category: "Inspiration"}, 
     {text: "The best way to get started is to quit talking and begin doing", category: "Motivation"},
      {text: "Don't let yesterday take up too much of today", category: "Life"}]; 
-     //random quotes
-     function showRandomQuote(){
-        const randomNumber = Math.floor(Math.random() * quotes.length);
-        const randomQuote = quotes[randomNumber];
-        quoteDisplay.innerHTML = `"${randomQuote.text}" - ${randomQuote.category}`;
+     
+     
+   //   random quotes
+    
+   //   function showRandomQuote(){
+   //      const randomNumber = Math.floor(Math.random() * quotes.length);
+   //      const randomQuote = quotes[randomNumber];
+   //      quoteDisplay.innerHTML = `"${randomQuote.text}" - ${randomQuote.category}`;
 
-     }
-     newQuoteBtn.addEventListener("click", showRandomQuote);
-     showRandomQuote();
+   //   }
+   //   newQuoteBtn.addEventListener("click", showRandomQuote);
+   //   showRandomQuote();
 
-     //display all quotes
+   //   display all quotes
 
-     function displayQuotes(){
-      listItem.innerHTML = "";
-      quotes.forEach((q) => {
-         const li = document.createElement("li");
-         li.textContent = `"${q.text}" - ${q.category}`;
-         listItem.appendChild(li);
-      })
-     }
+   //   function displayQuotes(){
+   //    listItem.innerHTML = "";
+   //    quotes.forEach((q) => {
+   //       const li = document.createElement("li");
+   //       li.textContent = `"${q.text}" - ${q.category}`;
+   //       listItem.appendChild(li);
+   //    })
+   //   }
 
      //user adding quotes
 
@@ -57,11 +61,18 @@ const importfile = document.getElementById("importFile");
      //update local storage
      localStorage.setItem("quotes", JSON.stringify(quotes));
 
-     displayQuotes();
-     showRandomQuote();
+     
+   //   populateCategories();
+   //   filterQuotes();
+
+   //   displayQuotes();
+   //   showRandomQuote();
 
      newQuoteText.value = "";
      newQuoteCategory.value = "";
+   //the one o ChatGPT
+     populateCategories();
+     filterQuotes();
 
     }
 
@@ -114,8 +125,68 @@ const importfile = document.getElementById("importFile");
      ExportBtn.addEventListener("click", exportQuotes);
    //   importBtn.addEventListener("click", importFromJsonFile);
 
-     displayQuotes();
-     showRandomQuote();
+   //   displayQuotes();
+   //   showRandomQuote();
+
+   //populate categories
+
+   function populateCategories(){
+      const categoryDrop = [... new Set(quotes.map(q => q.category))];
+      categoryFilter.innerHTML = `<option value="all"> All Category</option>`;
+      categoryDrop.forEach(cat => {
+         const option = document.createElement("option");
+         option.value = cat;
+         option.textContent = cat;
+         categoryFilter.appendChild(option);
+      })
+   }
+
+   // populateCategories();
+
+   //filter quotes based on category
+
+   function filterQuotes(){
+      const selectedCat = categoryFilter.value;
+      localStorage.setItem("selectedCat", selectedCat);
+      let filteredOption = selectedCat === "all" ? quotes : quotes.filter(q => q.category === selectedCat);
+      if(filteredOption.length === 0){
+         quoteDisplay.innerHTML = "No quotes avalaible for this category";
+
+      }else{
+         const randomQuoteShow = filteredOption[Math.floor(Math.random()) * filteredOption.length];
+         quoteDisplay.innerHTML = `"${randomQuoteShow.text}" - ${randomQuoteShow.category}`;
+      }
+   }
+
+   //Restore last selected filter from storage
+
+   function restoreFilter(){
+   const lastSelected = localStorage.getItem("selectedCat");
+   if(lastSelected && [... categoryFilter.options]
+      .some(opt => opt.value === lastSelected)
+   ){
+   categoryFilter.value = lastSelected;
+   }else{
+      categoryFilter.value = "all";
+   }
+   filterQuotes();
+}
+   // populateCategories();
+   // filterQuotes()
+
+   // addBtn.addEventListener("click", createAddQuoteForm);
+   // categoryFilter.addEventListener("change", filterQuotes);
+   
+   populateCategories();
+   restoreFilter();
+   addBtn.addEventListener("click", createAddQuoteForm);
+   // categoryFilter.addEventListener("change", filterQuotes);
+   
+
+
+
+
+  
 
  
 
